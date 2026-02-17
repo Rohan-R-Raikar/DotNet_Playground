898eb80a28f2e9b8281567cdc5abef4a62e1c3cf
Rohan R Raikar <rohanraikar7993@gmail.com>
2026-02-17 11:45:56 +0530

    Add ConsoleApp for StartPattern1outof22

---
+++ C#_Console_Apps/StartPattern1outof22/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization.Formatters;
+
+namespace StartPattern1outof22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
