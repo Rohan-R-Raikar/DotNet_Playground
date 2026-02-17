70452764cc14f9209b6ea74df92a238bc4d781f2
Rohan R Raikar <rohanraikar7993@gmail.com>
2026-02-17 12:02:31 +0530

    Add ConsoleApp for StarPattern4outof22

---
+++ C#_Console_Apps/StarPattern4outof22/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace StarPattern4outof22
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                for (int  j = 1; j <= i; j++)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
