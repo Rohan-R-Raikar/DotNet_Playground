40872721f42b86e6e1d1647ca3adac1fc3bb91f2
Rohan R Raikar <rohanraikar7993@gmail.com>
2026-02-17 15:19:10 +0530

    Add ConsoleApp for StarPattern9outof22

---
+++ C#_Console_Apps/StarPattern9outof22/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace StarPattern9outof22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 5;
+            for(int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < rows - i; j++)
+                {
+                    Console.Write("  ");
+                }
+                for (int j = 0; j < 2 * i + 1; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("  ");
+                }
+                for (int j = 0; j < (rows * 2) - (2 * i + 1); j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
