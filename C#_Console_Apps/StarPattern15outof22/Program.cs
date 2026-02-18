f563208e7d470a440a8aafddd6cecc60a9d41cde
Rohan R Raikar <rohanraikar7993@gmail.com>
2026-02-18 16:22:07 +0530

    Add ConsoleApp for StarPattern15outof22

---
+++ C#_Console_Apps/StarPattern15outof22/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace StarPattern15outof22
+{
+    class Program
+    {
+        /*
+         * ABCDE
+         * ABCD
+         * ABC
+         * AB
+         * A
+         */
+        static void Main(string[] args)
+        {
+            int row = 5;
+            for (int i = 0; i < row; i++)
+            {
+                char c = 'A';
+                for (int j = 0; j < row-i; j++)
+                {
+                    Console.Write(c);
+                    c++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
