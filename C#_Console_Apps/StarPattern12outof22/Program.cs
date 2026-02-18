689147f213b36d51d77edbe596d462ac6d2443f0
Rohan R Raikar <rohanraikar7993@gmail.com>
2026-02-18 10:51:09 +0530

    Update ConsoleApp for StarPattern12outof22

---
+++ C#_Console_Apps/StarPattern12outof22/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace StarPattern12outof22
+{
+    class Program
+    {
+        /*
+         * 1      1
+         * 12    21
+         * 123  321
+         * 12344321 
+         */
+        static void Main(string[] args)
+        {
+            int row = 6;
+            for (int i = 1; i < row; i++)
+            {
+
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write(i);
+                }
+                for(int j = 1; j < ((row * 2) - 1) - (i * 2); j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
