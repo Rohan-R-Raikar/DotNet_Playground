5b2813d9533c696b3b1ca1f31d41295f482e8c78
Rohan R Raikar <rohanraikar7993@gmail.com>
2026-02-19 10:46:18 +0530

    Update ConsoleApp for StarPattern18outof22

---
+++ C#_Console_Apps/StarPattern18outof22/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace StarPattern18outof22
+{
+    class Program
+    {
+        /*
+         * E
+         * DE
+         * CDE
+         * BCDE
+         * ABCDE
+         */
+        static void Main(string[] args)
+        {
+            int row = 5;
+            char lastChar = (char)('A' + row - 1);
+
+            for (int i = 0; i < row; i++)
+            {
+                char start = (char)(lastChar - i);
+
+                for (char c = start; c <= lastChar; c++)
+                {
+                    Console.Write(c);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
