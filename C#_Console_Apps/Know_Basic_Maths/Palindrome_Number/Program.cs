ec2208ff7dccc2da45238b276c0026ce2f86a319
Rohan R Raikar <rohanraikar7993@gmail.com>
2026-02-27 11:47:31 +0530

    Add palindrome number checker in C# with comments

---
+++ C#_Console_Apps/Know_Basic_Maths/Palindrome_Number/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Palindrome_Number
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a Number : ");
+            int num =  Convert.ToInt32(Console.ReadLine());
+
+            int OrginalNum = num;
+            int reverseNumber = 0;
+
+            while (num > 0)
+            {
+                reverseNumber = (reverseNumber * 10) + (num % 10);
+                num = num / 10;
+            }
+
+            if (reverseNumber == OrginalNum) Console.WriteLine("Number is Palindrome Number");
+            else Console.WriteLine("Number is not Regular Number"); 
+        }
+    }
+}
