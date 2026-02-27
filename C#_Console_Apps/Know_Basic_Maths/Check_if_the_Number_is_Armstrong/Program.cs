2d1b1a146273dc7c1143c4874c5a96a6a1f35292
Rohan R Raikar <rohanraikar7993@gmail.com>
2026-02-27 13:04:30 +0530

    Implement C# console app to verify Armstrong numbers

---
+++ C#_Console_Apps/Know_Basic_Maths/Check_if_the_Number_is_Armstrong/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Check_if_the_Number_is_Armstrong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int digits = num.ToString().Length;
+
+            int temp = num;
+            while (temp > 0)
+            {
+                int digit = temp % 10;
+                sum += (int)Math.Pow(digit, digits);
+                temp /= 10;
+            }
+
+            if (sum == num)
+                Console.WriteLine($"{num} is an Armstrong number.");
+            else
+                Console.WriteLine($"{num} is NOT an Armstrong number.");
+        }
+    }
+}
