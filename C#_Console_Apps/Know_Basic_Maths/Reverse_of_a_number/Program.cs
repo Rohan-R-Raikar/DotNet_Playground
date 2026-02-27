e43ec7c3610affbaf8b007a1cf5a964a339e3f10
Rohan R Raikar <rohanraikar7993@gmail.com>
2026-02-27 11:30:53 +0530

    Add program to reverse an integer using iterative digit extraction

---
+++ C#_Console_Apps/Know_Basic_Maths/Reverse_of_a_number/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Reverse_of_a_number
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number you want to reverse");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            // Variables to store last digit and reversed number
+            int ld = 0;
+            int revNum = 0;
+            while (num > 0)
+            {
+                // Get the last digit of the number
+                ld = num % 10;
+
+                // Append the last digit to the reversed number
+                revNum = (revNum * 10) + ld;
+
+                // Remove the last digit from the original number
+                num = num / 10;
+            }
+
+            Console.WriteLine("Your Reversed Number is : " + revNum);
+
+            Console.ReadKey();
+        }
+    }
+}
