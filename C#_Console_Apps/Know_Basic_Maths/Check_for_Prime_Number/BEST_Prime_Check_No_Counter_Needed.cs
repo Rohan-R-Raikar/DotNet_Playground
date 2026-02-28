a21614494e56ce4d0ac375d10d339f6b31aaf125
Rohan R Raikar <rohanraikar7993@gmail.com>
2026-02-28 10:39:12 +0530

    Prime Number with BEST Prime Check No Counter Needed

---
+++ C#_Console_Apps/Know_Basic_Maths/Check_for_Prime_Number/BEST_Prime_Check_No_Counter_Needed.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_for_Prime_Number
+{
+    // BEST Prime Check (No Counter Needed)
+    public static class BEST_Prime_Check_No_Counter_Needed
+    {
+        public static void CheckPrime(int Number)
+        {
+            if (Number <= 0)
+            {
+                Console.WriteLine("Not Prime");
+                return;
+            }
+
+            bool isPrime = true;
+
+            for (int i = 0; i*i <= Number; i++)
+            {
+                if (Number % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+                Console.WriteLine("Prime");
+            else
+                Console.WriteLine("Not Prime");
+        }
+    }
+}
