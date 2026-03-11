fc5819fb15fe4e84b1d0c504739c758ca286a878
Rohan R Raikar <rohanraikar7993@gmail.com>
2026-03-11 17:20:29 +0530

    chore: Learning Basics of xUnit for unit testing, What is AAA, What is Fact, What is Fat, How to create a xUnit Project, Writing one test case

---
+++ xUnitPractice/xUnitExample.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xUnitExample.Controllers;
+
+namespace xUnitExample.Tests.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void HomeController_Index_ValidResult()
+        {
+            //
+            // AAA
+            // Arrage -
+            HomeController controller = new HomeController();
+            string expectedResult = "I am in Home";
+
+            // Act -
+            string result = controller.Index();
+            
+            // Assert -
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ xUnitPractice/xUnitExample/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace xUnitExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public string Index()
+        {
+            return "I am in Home";
+        }
+    }
+}
